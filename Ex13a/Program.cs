225ec9aefe1b597a7e68f31c2ddc62987f601976
Александр <fedoseevale@gmail.com>
2022-10-02 16:27:38 +0400

    Ex13a done

---
+++ Ex13a/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+//Решение без использовния метода
+
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 10) > -10 && (number / 10) < 10)
+{
+    Console.WriteLine("У данного числа третьей цифры нет");
+}
+else if (((number / 10) > 0 && (number / 10) < 100) || ((number / 10) < 0 && (number / 10) > - 100))
+{
+    int thirdNumber = Math.Abs (number % 10);
+    Console.WriteLine($"Третья цифра данного числа: {thirdNumber}");
+}
+else
+{
+    while ((number > 999) || (number <-999))
+    {
+        number = Math.Abs(number / 10);
+    }
+    Console.WriteLine($"Третья цифра данного числа: {number % 10}");
+}
+
